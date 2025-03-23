2e45a2571341ad445ae6e7b824087abf593aa774
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-03-23 00:00:25 -0500

    Added a TextFile to Document tips or information I find useful for future use

--- obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d869067cda6c508181e2008a5d42d21e6987980d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92965317e3420a4f236c6035f289968befff57d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
