e13a435982693e0e0fbf295a38cad185124eb34f
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-03-23 00:13:46 -0500

    Created a MonsterSense model

--- obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6074c5654d8212e6382db162a9fa4bd0535c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81986233286f9cec88e9793145b60738437beda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
