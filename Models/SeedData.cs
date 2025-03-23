79d597544cb740c1f462f54af284748de0ec96f6
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-03-23 16:53:42 -0500

    Created the SeedData.cs in the Model Folder, and populated with code that creates a new Example Monster

---
+++ Models/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DungeonsAndDragonsMonsterManualCSharp.Data;
+using System;
+using System.Linq;
+
+namespace DungeonsAndDragonsMonsterManualCSharp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DungeonsAndDragonsMonsterManualCSharpContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<DungeonsAndDragonsMonsterManualCSharpContext>>()))
+            {
+              
+                if (context.Monster.Any())
+                {
+                    return; 
+                }
+                context.Monster.AddRange(
+                    new Monster
+                    {
+                        Name = "Example",
+                        Description = "Example",
+                        ArmourClass = 2,
+                        HitPoints = "1D3",
+                        HitDice = "1D5",
+                        ImageUrl = "Example.URL"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b45242429c95d30983501b222329193de515861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+888de625ef6837066aeb704acfdac60e0b38301b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
