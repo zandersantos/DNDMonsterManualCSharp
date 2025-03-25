c4b86e1c331da2f385be352d5860c20f2e926475
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-03-25 10:54:47 -0500

    Created the MonsterSense Bulk Insertion Query and populated the MonsterSense table

---
+++ Data/MonsterSenseBulkInsert.sql
@@ -0,0 +1,29 @@
+﻿/*
+Create a Temporary Table to store the CSV Data and then transfer the Data to the Actual MonserSense Table
+*/
+CREATE TABLE #TempMonsterSense (
+    SenseRange NVARCHAR(MAX),
+    MonsterId INT,
+    SenseId INT
+);
+
+BULK INSERT #TempMonsterSense
+FROM 'C:\Users\zainz\Desktop\DNDC#\DungeonsAndDragonsMonsterManualCSharp\DungeonsAndDragonsMonsterManualCSharp\Data\monstersensedatafile.csv'
+WITH (
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2 
+);
+
+INSERT INTO MonsterSense (SenseRange, MonsterId, SenseId)
+SELECT SenseRange, MonsterId, SenseId
+FROM #TempMonsterSense;
+
+DROP TABLE #TempMonsterSense;
+
+SELECT * FROM MonsterSense;
+DELETE FROM MonsterSense;
+DBCC CHECKIDENT ('MonsterSense, RESEED, 0');
+
+
+
--- obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2aefafc5ec373657526a3a9e8920e3f9c2f380f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ecedaa50d720e49f0321cc58f494ad9541a8e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
