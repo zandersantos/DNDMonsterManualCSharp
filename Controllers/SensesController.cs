48384839349644aa9a54393486645a979747e8d7
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-04-03 20:23:28 -0500

    Updated the Senses and Actions Controllers to include Admin attributes to the Create, Edit and Delete actions

--- Controllers/SensesController.cs
+++ Controllers/SensesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DungeonsAndDragonsMonsterManualCSharp.Data;
 using DungeonsAndDragonsMonsterManualCSharp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
 {
@@ -44,6 +45,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
         }
 
         // GET: Senses/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
@@ -53,6 +55,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,SenseType")] Sense sense)
         {
@@ -66,6 +69,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
         }
 
         // GET: Senses/Edit/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -85,6 +89,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,SenseType")] Sense sense)
         {
@@ -117,6 +122,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
         }
 
         // GET: Senses/Delete/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -136,6 +142,7 @@ namespace DungeonsAndDragonsMonsterManualCSharp.Controllers
 
         // POST: Senses/Delete/5
         [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
--- obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonsAndDragonsMonsterManualCSharp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0ff80043a0b61132bb93bacab1d6a8f72e072f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e85da70885438728734ef0633d5ed0c949d38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonsAndDragonsMonsterManualCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
